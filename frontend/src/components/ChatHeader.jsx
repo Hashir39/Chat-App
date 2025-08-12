e380fb342ecb7f123423dab391e5d19a8a8000d8
Hashir39 <126954940+Hashir39@users.noreply.github.com>
2025-08-13 00:10:16 +0500

    Update ChatHeader.jsx

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -8,7 +8,7 @@ const ChatHeader = () => {
 
   return (
     <div className="p-2.5 border-b border-base-300">
-      <div className="flex item-center justify-between">
+      <div className="flex itemd-center justify-between">
         <div className="flex items-center gap-3">
           {/* Avatar */}
           <div className="avatar">
